5d64c37e8d90625e245d65d1ba4061f7a2147a42
seunhub007 <sadiboseun@yahoo.com>
2023-12-01 11:12:45 +0100

    runner commit

--- cid.sh
+++ cid.sh
@@ -18,12 +18,11 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 ## install runner
-mkdir /home/ubuntu/actions-runner 
-cd /home/ubuntu/actions-runner
+mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
-echo -ne "\n" | ./config.sh --url https://github.com/seunhub007/SimplePersonal --token A4ZVN35BHKSYNUV765DKXATFMDFG6
+echo -ne "\n" | ./config.sh --url https://github.com/seunhub007/devjob --token A4ZVN36BCTWMS5CB3LV6LRLFNG2UO
 ./run.sh&
 
 
